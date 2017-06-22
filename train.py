80fec690d016cae28474dd68203f4588050108e8
chasingbob <dries139@gmail.com>
2017-06-22 15:57:00 +0200

    big train.py refactoring

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 from skimage import color, io
 from scipy.misc import imresize
+import click
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -32,11 +33,11 @@ print(file_count)
 
 image_size = 128
 
-file_count = 21000
+file_count = 4000
 allX = np.zeros((file_count, image_size, image_size, 3), dtype='float64')
 ally = np.zeros(file_count)
 count = 0
-for f in cat_files[:10500]:
+for f in cat_files[:2000]:
     try:
         img = io.imread(f)
         new_img = imresize(img, (image_size, image_size, 3))
@@ -47,10 +48,10 @@ for f in cat_files[:10500]:
     except:
         continue
 
-for f in dog_files[:10500]:
+for f in dog_files[:2000]:
     try:
         img = io.imread(f)
-        new_img = imresize(img, (image_size, image_size, 3))
+            new_img = imresize(img, (image_size, image_size, 3))
         new_img = np.array(new_img) / 255.
         allX[count] = np.array(new_img)
         ally[count] = 1
@@ -61,7 +62,7 @@ for f in dog_files[:10500]:
 file_count = count
         
 # test-train split   
-X_train, X_val, Y_train, Y_val = train_test_split(allX, ally, test_size=0.04, random_state=43)
+X_train, X_val, Y_train, Y_val = train_test_split(allX, ally, test_size=0.5, random_state=43)
 X_val, X_test, y_val, y_test = train_test_split(X_val, Y_val, test_size=0.5, random_state=97)
 print('Train/Val/Test split:')
 print('X_train: {} {}'.format(X_train.shape[0], X_train.shape))
@@ -72,8 +73,7 @@ now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
 root_path = 'tf_logs'
 logdir = '{}/run-{}/'.format(root_path, now)
 
-X = tf.placeholder(tf.float32, shape=[None, image_size, image_size, 3], name="X")
-y = tf.placeholder(tf.int32, shape=[None], name="y")
+
 
 def conv_maxpool(inputs, num_filters=32, name='conv-maxpool'):
     with tf.name_scope(name):
@@ -87,68 +87,70 @@ def conv_maxpool(inputs, num_filters=32, name='conv-maxpool'):
         pool = tf.nn.max_pool(conv, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
         return pool
 
-with tf.name_scope('model'):
-    convmax1 = conv_maxpool(inputs=X, num_filters=32, name='conv-max-1')
-    convmax2 = conv_maxpool(inputs=convmax1, num_filters=64, name='conv-max-2')
-    convmax3 = conv_maxpool(inputs=convmax2, num_filters=128, name='conv-max-3')
-    convmax4 = conv_maxpool(inputs=convmax3, num_filters=128, name='conv-max-4')
-    #convmax5 = conv_maxpool(inputs=convmax4, num_filters=128, name='conv-max-5')
-    #convmax6 = conv_maxpool(inputs=convmax5, num_filters=128, name='conv-max-6')
+#avg_acc = []
+#train_accs = []
+#test_accs = []
 
-    print('Convmax 4 shape: {}'.format(convmax4.shape))
+@click.command()
+@click.option('--epochs', default=3, help='number of epochs to train model')
+@click.option('--batch_size', default=16, help='number of images to go into each training batch')
+def train(epochs, batch_size):
+    print('Start Training...')
 
-    with tf.name_scope('flat'):
-        pool_flat = tf.reshape(convmax4, shape=[-1, 128 * 8 * 8])
+    with tf.name_scope('placeholders'):
+        X = tf.placeholder(tf.float32, shape=[None, image_size, image_size, 3], name="X")
+        y = tf.placeholder(tf.int32, shape=[None], name="y")
 
-    with tf.name_scope('fc-1'):
-        dense = tf.layers.dense(inputs=pool_flat, units=1024, activation=tf.nn.relu)
-    with tf.name_scope('drop-out-1'):
-        dropout = tf.layers.dropout(inputs=dense, rate=0.5)
+    with tf.name_scope('model'):
+        convmax1 = conv_maxpool(inputs=X, num_filters=32, name='conv-max-1')
+        convmax2 = conv_maxpool(inputs=convmax1, num_filters=64, name='conv-max-2')
+        convmax3 = conv_maxpool(inputs=convmax2, num_filters=128, name='conv-max-3')
+        convmax4 = conv_maxpool(inputs=convmax3, num_filters=128, name='conv-max-4')
+        #convmax5 = conv_maxpool(inputs=convmax4, num_filters=128, name='conv-max-5')
+        #convmax6 = conv_maxpool(inputs=convmax5, num_filters=128, name='conv-max-6')
 
-    # Logits Layer
-    with tf.name_scope('logits-1'):
-        logits = tf.layers.dense(inputs=dropout, units=2)
+        print('Convmax 4 shape: {}'.format(convmax4.shape))
 
-with tf.name_scope('ops-1'):
-    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
-    loss = tf.reduce_mean(xentropy)
-    optimizer = tf.train.AdamOptimizer()
-    #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    training_op = optimizer.minimize(loss)
+        with tf.name_scope('flat'):
+            pool_flat = tf.reshape(convmax4, shape=[-1, 128 * 8 * 8])
 
-with tf.name_scope('summary'):
-    # accuracy
-    correct = tf.nn.in_top_k(logits, y, 1)
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name='accuracy')
-    # This is a bit of a hack to get TensorBoard to display graphs on same chart
-    #acc_summary = tf.summary.scalar('acc', accuracy)
+        with tf.name_scope('fc-1'):
+            dense = tf.layers.dense(inputs=pool_flat, units=1024, activation=tf.nn.relu)
+        with tf.name_scope('drop-out-1'):
+            dropout = tf.layers.dropout(inputs=dense, rate=0.5)
 
-    current_acc = tf.Variable(0.0, name="current_acc")
-    acc_summary = tf.summary.scalar('acc', current_acc)
-    val_file_writer = tf.summary.FileWriter('tf_logs/val', tf.get_default_graph())
-    train_file_writer = tf.summary.FileWriter('tf_logs/train', tf.get_default_graph())
+        # Logits Layer
+        with tf.name_scope('logits-1'):
+            logits = tf.layers.dense(inputs=dropout, units=2)
 
-    write_op = tf.summary.merge_all()
+    with tf.name_scope('ops-1'):
+        xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+        loss = tf.reduce_mean(xentropy)
+        optimizer = tf.train.AdamOptimizer()
+        #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        training_op = optimizer.minimize(loss)
 
+    with tf.name_scope('summary'):
+        # accuracy
+        correct = tf.nn.in_top_k(logits, y, 1)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name='accuracy')
+        # This is a bit of a hack to get TensorBoard to display graphs on same chart
+        #acc_summary = tf.summary.scalar('acc', accuracy)
 
-# Init
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
+        current_acc = tf.Variable(0.0, name="current_acc")
+        acc_summary = tf.summary.scalar('acc', current_acc)
+        val_file_writer = tf.summary.FileWriter('tf_logs/val', tf.get_default_graph())
+        train_file_writer = tf.summary.FileWriter('tf_logs/train', tf.get_default_graph())
 
-n_epochs = 150
-batch_size = 128
+        write_op = tf.summary.merge_all()
 
-avg_acc = []
-train_accs = []
-test_accs = []
-
-
-def train():
-    print('Training started...')
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    
     init.run()
     step = 0
     prev_best = 0
-    for epoch in range(n_epochs):
+    for epoch in range(epochs):
         for i in range(len(X_train) // batch_size):
             X_train_batch = fetch_batch(X_train, i, batch_size)
             Y_train_batch = fetch_batch(Y_train, i, batch_size)
@@ -184,13 +186,29 @@ def train():
                     print('... save')
                     prev_best = temp_acc
                     save_path = saver.save(sess, "./model-{}-{:2.2f}.ckpt".format(epoch, temp_acc))
+        print('Epoch: {}'.format(epoch))
+    print('No more epochs!!')
+
+    val_file_writer.close()
 
 
 def load_model():
     saver.restore(sess, './model-9-0.74.ckpt')
 
-        
-def test_predict():
+def test_accuracy():
+    print('*** Test acc ***')
+    test_accs = []
+    for i in range(len(X_test) // batch_size):
+        X_test_batch = fetch_batch(X_test, j, batch_size)
+        y_test_batch = fetch_batch(y_test, j, batch_size)
+
+        test_acc = sess.run(accuracy, feed_dict={X:X_test_batch, y: y_test_batch})
+        test_accs.append(test_acc)
+    
+    print('Model accuracy against test set: {}'.format(sum(test_accs)/len(test_accs)))
+               
+     
+def test_predict_visual():
 # visualize predictions
     fig=plt.figure()
     fig.set_figheight(18)
@@ -228,10 +246,18 @@ def test_predict():
         _sub.axes.get_yaxis().set_visible(False)
     plt.show()
 
-with tf.Session() as sess:
-    train()
-#    load_model()
-#    test_predict()
+    
+
+if __name__ == "__main__":
+    print('__main__')
+    with tf.Session() as sess:
+        train()
+    #    load_model()
+        #print('Test acc now...')
+        #test_accuracy()
+    #    test_predict_visual(sess)
+
+    print('__main__')
+    
 
     
-val_file_writer.close()
